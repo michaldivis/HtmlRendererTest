045c52f1982c76a09222f1368810e00469e96f81
Michal Diviš <A408674@dekra.org>
2019-12-05 15:24:11 +0100

    Add project files.

---
+++ HtmlRendererTest.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using TheArtOfDev.HtmlRenderer.WPF;
+
+namespace HtmlRendererTest.Wpf
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _htmlTooltip;
+        public string HtmlTooltip
+        {
+            get => _htmlTooltip;
+            set
+            {
+                _htmlTooltip = value;
+                OnPropertyChanged(nameof(HtmlTooltip));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            HtmlTooltip = "Isn't <b>this</b> kinda <br> cool?";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
